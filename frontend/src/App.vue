33767368d8dd257c1cad3da6d918d35f1ee2f2bc
Dharmiksarvaiya24 <dharmiksarvaiya07@gmail.com>
2025-03-13 01:01:16 +0530

    froentend created

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <login/> <!-- login related-->
+  </div>
+</template>
+
+<script>
+import login from "./components/login.vue"; 
+
+export default {
+  components: {
+    login, 
+  },
+};
+</script>
---
+++ frontend/src/components/login.vue
@@ -0,0 +1,42 @@
+<!-- it contains login page related code-->
+<template>
+    <div>
+        <h1>Login</h1>
+        <form>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" />
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" />
+            <button type="submit">Login</button>
+
+        </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "login",
+  };
+  </script>
+  <style scoped>
+  div{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+  }
+   form{
+    border: 1px solid black;
+    padding: 20px;
+    margin: 20px;
+    width: 250px;
+    display: flex;
+    flex-direction: column;
+   }
+   label, input, button {
+    margin-bottom: 10px;
+}
+  </style>
+ 
+  
